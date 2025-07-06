d29c6311932c5e3057db63f9afa77e7198ce915e
Olivia <oliviawolfie@pm.me>
2025-07-07 01:40:45 +0200

    Add dedicated pypi README

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,14 +2,19 @@
 
 set -e
 
+# Set up directory
 cd /opt
-sudo mkdir miarchive 
-
+sudo mkdir miarchive
 sudo chown $USER miarchive
 cd miarchive
 
+# Set up venv
 python3 -m venv env
 source env/bin/activate
 
+# Install MIA
 pip3 install miarchive
+
+# Set up environment
+miarchive setup
 
